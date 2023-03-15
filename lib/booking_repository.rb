07e1307592b3c5763633fdcfb8d818523f581b28
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-15 10:34:35 +0000

    made booking repo with all and find method

---
+++ lib/booking_repository.rb
@@ -0,0 +1,30 @@
+class BookingRepository
+  def all
+   query = "SELECT * FROM bookings;"
+   result_set = DatabaseConnection.exec_params(query, [])
+   bookings = []
+   result_set.each do |record|
+    booking = Booking.new
+    booking.id = record["id"].to_i
+    booking.booking_date = record["booking_date"]
+    booking.booking_status = record["booking_status"]
+    booking.property_id = record["property_id"].to_i
+    booking.customer_id = record["customer_id"].to_i
+    bookings.push(booking)
+   end
+   return bookings
+  end
+
+  def find(id)
+    sql = 'SELECT id, booking_date, booking_status, property_id, customer_id FROM bookings WHERE id = $1;'
+    result_set = DatabaseConnection.exec_params(sql,[id])
+    booking = Booking.new
+    booking.id = result_set[0]['id'].to_i
+    booking.booking_date = result_set[0]['booking_date']
+    booking.booking_status = result_set[0]['booking_status']
+    booking.property_id = result_set[0]['property_id'].to_i
+    booking.customer_id = result_set[0]['customer_id'].to_i
+    return booking
+  end
+ 
+end
